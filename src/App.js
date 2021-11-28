1889122fcb53581df22030e43d742d73176807a0
Josseline <guardadojosseline@gmail.com>
2021-11-28 18:12:57 -0500

    Added Components and Router

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
 import logo from './josseline.jpg';
 import './App.css';
+import Projects from "./components/Projects"
+import Home from "./components/Home"
+import AboutMe from './components/AboutMe';
+import ContactMe from './components/ContactMe';
+import {Routes, Route, Link} from 'react-router-dom'
 
-function App() {
+
+function App() { 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-         "My name is Josseline and I'm going to master React!"
-        </p>
-        <a
-          className="App-link"
-          href="https://github.com/JosselineG"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          "Check Me Out!"
-        </a>
-      </header>
+     
+     <Routes>
+       <Route path="projects" elements={<Projects/>} />
+       <Route path="/" element={<Home/>}/>
+       <Route path="About Me" element={<AboutMe/>}/>
+       <Route path="Contact Me" element={<ContactMe/>}/>
+     </Routes>
+
+
+
     </div>
   );
 }
