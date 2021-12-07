b7d3184965a8168e282bff8f6a104aa3fd41a300
Josseline <guardadojosseline@gmail.com>
2021-12-07 17:26:58 -0500

    final work

--- src/components/pages/Project2.js
+++ src/components/pages/Project2.js
@@ -1,9 +1,16 @@
 export default function Project2(){ 
 
-return(
-
-    <h1>Project 2</h1>
-)
+    return(
+    <div className="proj2"> 
+        
+        <h1>Project 2</h1>
+        <p>Name of Project:</p>
+        <p>Link:</p>
+        <p>Description:</p>
     
+    </div>
 
-}
+    )
+        
+    
+    }
--- src/components/pages/Project3.js
+++ src/components/pages/Project3.js
@@ -1,8 +1,15 @@
 export default function Project3(){ 
 
     return(
-    
+    <div className="proj3"> 
+        
         <h1>Project 3</h1>
+        <p>Name of Project:</p>
+        <p>Link:</p>
+        <p>Description:</p>
+    
+    </div>
+
     )
         
     
